8c78c1a1e65e5c2cc26c6e962198199f1b9a7907
Ugur Guney <ugurguney@gmail.com>
2024-11-28 14:56:22 -0500

    open a static window that's closed when ESC is pressed
    conditional debug vs release print. Print something to console  using Windows Api.

--- smol/main.cpp
+++ smol/main.cpp
@@ -1,10 +1,40 @@
+#define WIN32_LEAN_AND_MEAN
+#define WIN32_EXTRA_LEAN
+#include <Windows.h>
 #ifdef _DEBUG
 #include <print>
 #endif
 
 int main() {
+  const HANDLE kStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+  const int kWidth = 1024;
+  const int kHeight = 768;
+
 #ifdef _DEBUG
-  std::println("Hello, World!");
+  std::println("Hi, Debug!");
+#elif NDEBUG
+  {
+    const char message[] = "Hi, Optimized!\n";
+    WriteConsoleA(kStdOut, message, sizeof(message) - 1, nullptr, nullptr);
+  }
 #endif
-  return 1;
+
+  // By default STATIC window will have title also drawn inside the window. But that'd be erased when window is cleared
+  const HWND hWindow = CreateWindowA(
+      "STATIC", "Smol", WS_OVERLAPPED,
+      CW_USEDEFAULT,
+      CW_USEDEFAULT,
+      kWidth, kHeight, nullptr, nullptr, nullptr, nullptr);
+  ShowCursor(0);  // Hides mouse cursor over the window
+  ShowWindow(hWindow, SW_SHOW);
+  UpdateWindow(hWindow);
+
+  while (!GetAsyncKeyState(VK_ESCAPE)) {
+  }
+
+  {
+    const char message[] = "Bye!\n";
+    WriteConsoleA(kStdOut, message, sizeof(message) - 1, nullptr, nullptr);
+  }
+  ExitProcess(0);
 }
