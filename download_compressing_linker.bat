720f22abee8225fb9bb16805e16f71f185c05705
Ugur Guney <ugurguney@gmail.com>
2024-11-30 02:37:31 -0500

    Remove 64-bit compressed config

--- download_compressing_linker.bat
+++ download_compressing_linker.bat
@@ -5,12 +5,10 @@ set URL=https://github.com/runestubbe/Crinkler/releases/download/v2.3/crinkler23
 set ZIPFILE=%~dp0crinkler23.zip
 set THISFOLDER=%~dp0
 
-REM PowerShell -Command "Invoke-WebRequest -Uri '%URL%' -OutFile '%ZIPFILE%'"
+PowerShell -Command "Invoke-WebRequest -Uri '%URL%' -OutFile '%ZIPFILE%'"
+PowerShell -Command "Expand-Archive -Path '%ZIPFILE%' -DestinationPath '%THISFOLDER%' -Force"
+PowerShell -Command "Rename-Item -Path '%THISFOLDER%\crinkler23\Win32\Crinkler.exe' -NewName 'link.exe' -Force"
+PowerShell -Command "Rename-Item -Path '%THISFOLDER%\crinkler23\Win64\Crinkler.exe' -NewName 'link.exe' -Force"
 
-REM PowerShell -Command "Expand-Archive -Path '%ZIPFILE%' -DestinationPath '%THISFOLDER%' -Force"
-
-REM PowerShell -Command "Copy-Item -Path '%THISFOLDER%crinkler23\Win32\Crinkler.exe' -Destination '%THISFOLDER%' -Force"
-
-PowerShell -Command "Rename-Item -Path 'Crinkler.exe' -NewName 'link.exe' -Force"
 
 endlocal
