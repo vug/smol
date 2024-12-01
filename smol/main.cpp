71d1607341482fa4927f252ff6982d44093c2698
Ugur Guney <ugurguney@gmail.com>
2024-11-30 23:49:52 -0500

    Revert "use glad to bring OpenGL -> 5KB"
    This reverts commit 1076e8262c9be2dc66b27fb7a8cb114752cb95c0.

--- smol/main.cpp
+++ smol/main.cpp
@@ -4,68 +4,6 @@
 #ifdef _DEBUG
 #include <print>
 #endif
-#include <glad/gl.h>
-
-void SetPixelFormatDescriptor(HDC hdc) {
-  PIXELFORMATDESCRIPTOR pfd = {
-      sizeof(PIXELFORMATDESCRIPTOR),
-      1,
-      PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER,
-      PFD_TYPE_RGBA,
-      32,
-      0,
-      0,
-      0,
-      0,
-      0,
-      0,
-      0,
-      0,
-      0,
-      0,
-      0,
-      0,
-      0,
-      24,
-      8,
-      0,
-      PFD_MAIN_PLANE,
-      0,
-      0,
-      0,
-      0};
-  int format = ChoosePixelFormat(hdc, &pfd);
-  SetPixelFormat(hdc, format, &pfd);
-}
-
-void CreateOpenGLContext(HWND hWnd) {
-  HDC hdc = GetDC(hWnd);
-
-  // Set pixel format for the window's device context.
-  SetPixelFormatDescriptor(hdc);
-
-  // Create a legacy OpenGL context for bootstrapping.
-  HGLRC tempContext = wglCreateContext(hdc);
-  if (!tempContext) {
-    MessageBoxA(NULL, "Failed to create temporary context", "Error", MB_OK);
-    ExitProcess(0);
-  }
-
-  // Make the temporary context current.
-  wglMakeCurrent(hdc, tempContext);
-
-  // Load GLAD functions using the internal loader.
-  const int version = gladLoaderLoadGL();
-  if (!version) {
-    MessageBoxA(NULL, "Failed to initialize GLAD", "Error", MB_OK);
-    ExitProcess(0);
-  } else {
-#ifdef _DEBUG
-    std::println("version {}.{}", GLAD_VERSION_MAJOR(version),  GLAD_VERSION_MINOR(version));
-#endif
-  }
-}
-
 
 int main() {
   const HANDLE kStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -90,8 +28,6 @@ int main() {
   ShowCursor(0);  // Hides mouse cursor over the window
   ShowWindow(hWindow, SW_SHOW);
   UpdateWindow(hWindow);
-
-  CreateOpenGLContext(hWindow);
 
   while (!GetAsyncKeyState(VK_ESCAPE)) {
   }
