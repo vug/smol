3a503d699f292d831feb89346b8eb45bdf863a11
Ugur Guney <ugurguney@gmail.com>
2024-12-28 01:14:45 -0500

    simple vertex color shader without camera

--- smol/assets/compile_shaders_to_spirv.bat
+++ smol/assets/compile_shaders_to_spirv.bat
@@ -4,4 +4,7 @@ setlocal
 glslangValidator.exe --target-env opengl --client opengl100 --glsl-version 460 --entry-point main -Os -o tri_v.hpp --variable-name vertex_shader_bin triangle_without_vbo.vert
 glslangValidator.exe --target-env opengl --client opengl100 --glsl-version 460 --entry-point main -Os -o tri_f.hpp --variable-name fragment_shader_bin triangle_without_vbo.frag
 
+glslangValidator.exe --target-env opengl --client opengl100 --glsl-version 460 --entry-point main -Os -o vert_col_v.hpp --variable-name vertex_shader_bin vertex_color.vert
+glslangValidator.exe --target-env opengl --client opengl100 --glsl-version 460 --entry-point main -Os -o vert_col_f.hpp --variable-name fragment_shader_bin vertex_color.frag
+
 endlocal
